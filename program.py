ce1492fe81b871bbbe9d3ba553619c8bb3db1726
noah <90880131+noahsmiths@users.noreply.github.com>
2024-09-05 16:13:12 -0400

    Update program.py

--- program.py
+++ program.py
@@ -1,15 +1,30 @@
+import importlib.util
 import os
 
 def check():
+    # Check if bs4 is installed
+    package_name = 'bs4'
+
+    spec = importlib.util.find_spec(package_name)
+    if spec is None:
+        print(f"{package_name} is not installed")
+    else:
+        print("beautifulsoup4 successfully installed!")
+
     # Check environment variable
-    if not 'COMP290' in os.environ:
-        print("No COMP290 environment variable set. Make sure it's capitalized!")
-        return
-    if not os.environ['COMP290'] == "SUPER_COOL_VALUE":
-        print(f"COMP290 environment variable should be set to SUPER_COOL_VALUE, but is instead: {os.environ['COMP290']}")
+    if 'COMP290' in os.environ:
+        env_var_value = os.environ['COMP290']
+        if env_var_value == 'fb009':
+            print(f"COMP290 environment variable set correctly!")
+        else:
+            print(
+                f"COMP290 environment variable set to '{os.environ['COMP290']}' but should be 'fb009'"
+            )
+    else:
+        print("COMP290 environment variable not set.")
 
     # Check file structure
-    if not os.getcwd() == "/project":
-        print(f"This program should have a working directory of /project, but instead is {os.getcwd()}")
+    if not os.getcwd() == "/a03":
+        print(f"This program should have a working directory of /a03, but instead is {os.getcwd()}")
 
 check()
